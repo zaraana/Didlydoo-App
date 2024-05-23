497c4f87566b5bf7b677cb8d947678ce15ac5e66
zaraana <zara.zara92@outlook.be>
2024-05-23 14:09:23 +0200

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,9 @@
 import { getEvents } from "./components/displayEvents.js";
 import { add_event } from "./components/add_event_modal.js";
+import { initializeDarkMode } from './components/darkMode.js';
+
 
 add_event();
 getEvents();
+
+document.addEventListener("DOMContentLoaded", initializeDarkMode);
