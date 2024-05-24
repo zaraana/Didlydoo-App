c98f8805b511ac9656ca762c3c2fe0980076320b
zaraana <zara.zara92@outlook.be>
2024-05-24 15:02:50 +0200

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,4 @@
+
 import { getEvents } from "./components/displayEvents.js";
 import { add_event } from "./components/add_event_modal.js";
 import { initializeDarkMode } from './components/darkMode.js';
