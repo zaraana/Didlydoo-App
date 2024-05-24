e4f0687f6e9e2338cc6aebc7120d2c24487dcb3c
Caroline <deconinck.caro@gmail.com>
2024-05-24 10:02:55 +0200

    blocked dates prior to today

--- assets/JS/components/add_event_modal.js
+++ assets/JS/components/add_event_modal.js
@@ -1,48 +1,57 @@
 import { postEvent } from "./post_event.js";
 import { affiche_dateListe } from "./afficheModalDateList.js";
 
-const event_modal = document.getElementById('addEvent_modal');
-const addEvent_button = document.getElementById('add-event');
-const input_name = document.getElementById('event_name');
-const input_author = document.getElementById('author');
-const input_date = document.getElementById('dates');
-const button_AddDate = document.getElementById('add_date');
-const input_description = document.getElementById('description');
-const submit_addEvent = document.getElementById('submit_addEvent');
-const date_list =document.getElementById('liste_date');
+const event_modal = document.getElementById("addEvent_modal");
+const addEvent_button = document.getElementById("add-event");
+const input_name = document.getElementById("event_name");
+const input_author = document.getElementById("author");
+const input_date = document.getElementById("dates");
+const button_AddDate = document.getElementById("add_date");
+const input_description = document.getElementById("description");
+const submit_addEvent = document.getElementById("submit_addEvent");
+const date_list = document.getElementById("liste_date");
 
-export function add_event(){
+export function add_event() {
+  addEvent_button.addEventListener("click", (event) => {
+    if (
+      event_modal.style.display === "none" ||
+      event_modal.style.display === ""
+    ) {
+      event_modal.style.display = "flex";
+      const addDateField = document.querySelector("#dates");
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = String(today.getMonth() + 1).padStart(2, "0");
+      const day = String(today.getDate()).padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+      console.log(formattedDate);
+      addDateField.setAttribute("min", formattedDate);
+    } else {
+      event_modal.style.display = "none";
+    }
+  });
 
-    addEvent_button.addEventListener('click',event=>{
+  let tab = [];
+  button_AddDate.addEventListener("click", (event) => {
+    //ajoute la date au tab
+    tab.push(input_date.value);
+    input_date.value = "";
 
-        if (event_modal.style.display === 'none' || event_modal.style.display === '' ) {
-            event_modal.style.display = "flex";
-        }else{
-            event_modal.style.display = "none";
-        }
-    
-    })  
+    //affiche la list des date avc une fct pour supp
+    affiche_dateListe(tab, date_list);
+  });
 
-    let tab=[];
-    button_AddDate.addEventListener('click',event=>{
+  submit_addEvent.addEventListener("click", (event) => {
+    postEvent(
+      input_name.value,
+      input_author.value,
+      tab,
+      input_description.value
+    );
 
-        //ajoute la date au tab 
-        tab.push(input_date.value);
-        input_date.value='';
-
-        //affiche la list des date avc une fct pour supp
-        affiche_dateListe(tab,date_list);
-
-    })
-
-    submit_addEvent.addEventListener('click',event=>{
-        postEvent(input_name.value,input_author.value,tab,input_description.value);
-
-        input_name.innerHTML='';
-        input_author.innerHTML='';
-        tab=[];
-        input_description.innerHTML='';
-
-    })
-
+    input_name.innerHTML = "";
+    input_author.innerHTML = "";
+    tab = [];
+    input_description.innerHTML = "";
+  });
 }
--- assets/JS/components/modifModal.js
+++ assets/JS/components/modifModal.js
@@ -1,39 +1,44 @@
 import { modifEvent } from "./patch_event.js";
 import { affiche_dateListe } from "./afficheModalDateList.js";
 
-export function modifModal(id,prefill){
+export function modifModal(id, prefill) {
+  const modifName = document.getElementById("modif_name");
+  const modifAuthor = document.getElementById("modif_author");
+  const modifDescription = document.getElementById("modif_description");
+  const add_dateButton = document.getElementById("add_dateButton");
+  const add_datesInput = document.getElementById("add_newDate");
+  const div_addedDate = document.getElementById("dates_added");
 
-    const modifName = document.getElementById('modif_name');
-    const modifAuthor = document.getElementById('modif_author');
-    const modifDescription =document.getElementById('modif_description');
-    const add_dateButton =document.getElementById('add_dateButton');
-    const add_datesInput = document.getElementById('add_newDate');
-    const div_addedDate =document.getElementById('dates_added');
+  const modal = document.getElementById("modifyEvent");
+  if (modal.style.display === "" || modal.style.display === "none") {
+    modal.style.display = "flex";
+  } else {
+    modal.style.display = "none";
+  }
 
-    const modal=document.getElementById('modifyEvent');
-    if(modal.style.display ==='' || modal.style.display === 'none'){
-        modal.style.display='flex';
-    }else{
-        modal.style.display ='none'
-    }
+  const dateField = document.querySelector("#add_newDate");
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate);
+  dateField.setAttribute("min", formattedDate);
 
-    // prefill stuff
-    modifName.value=prefill[0].name;
-    modifAuthor.value=prefill[0].author;
-    modifDescription.value=prefill[0].description;
-    
-    let tab_newDate = [];
+  // prefill stuff
+  modifName.value = prefill[0].name;
+  modifAuthor.value = prefill[0].author;
+  modifDescription.value = prefill[0].description;
 
-    add_dateButton.addEventListener('click',event=>{
+  let tab_newDate = [];
 
-        tab_newDate.push(add_datesInput.value);
-        
-        affiche_dateListe(tab_newDate,div_addedDate);
+  add_dateButton.addEventListener("click", (event) => {
+    tab_newDate.push(add_datesInput.value);
 
-        add_datesInput.value='';
-    })
+    affiche_dateListe(tab_newDate, div_addedDate);
 
-    
-    modifEvent(id,tab_newDate);
+    add_datesInput.value = "";
+  });
 
+  modifEvent(id, tab_newDate);
 }
