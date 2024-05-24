02e833518fd4f36606539c1d623b684231c298b2
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-05-24 09:52:32 +0200

    update date in modif modal

--- assets/JS/components/modifModal.js
+++ assets/JS/components/modifModal.js
@@ -26,13 +26,17 @@ export function modifModal(id,prefill){
 
     add_dateButton.addEventListener('click',event=>{
 
-        tab_newDate.push(add_datesInput.value);
+        if(add_datesInput !==''){
+
+            tab_newDate.push(add_datesInput.value);
         
-        affiche_dateListe(tab_newDate,div_addedDate);
+            affiche_dateListe(tab_newDate,div_addedDate);
+    
+            add_datesInput.value='';
 
-        add_datesInput.value='';
+        }
     })
-
+    
     
     modifEvent(id,tab_newDate);
 
